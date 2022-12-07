35ec55c12b3c589b118675b690e14851e8145508
LavaCourage07 <lava07@163.com>
2022-12-07 12:24:15 +0800

    test1

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -17,7 +17,7 @@ export default class Index extends Component {
   render() {
     return (
       <View className='index'>
-        <Text>Hello world!Lava!</Text>
+        <Text>Hello world! Lava, welcome to Cat Community ~</Text>
       </View>
     )
   }
